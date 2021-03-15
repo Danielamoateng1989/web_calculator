bea3edc1b3b9fc4c93b46f5397c79d44f4dd5275
Daniel Amoateng <Danielamoateng81@yahoo.com>
2021-03-14 23:28:42 -0400

    Initial commit

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function divideBy()
         num2 = document.getElementById("secondNumber").value;
 document.getElementById("result").innerHTML = num1 / num2;
 }
+
+function addition() 
+{ 
+        var  num1 = document.getElementById("firstNumber").value;
+        var  num2 = document.getElementById("secondNumber").value;
+		var sum = 0;
+   sum = document.getElementById("result").innerHTML = Number(num1) + Number(num2);
+}
+
+function subtraction() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 - num2;
+}
+
